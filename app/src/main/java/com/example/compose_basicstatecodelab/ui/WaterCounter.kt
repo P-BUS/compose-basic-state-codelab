70f6c71201567930cc0b4c3832aefeca54a55baa
P-BUS <pavlo.buslowski@gmail.com>
2023-04-19 19:53:20 +0300

    added internal state to water counter

--- app/src/main/java/com/example/compose_basicstatecodelab/ui/WaterCounter.kt
+++ app/src/main/java/com/example/compose_basicstatecodelab/ui/WaterCounter.kt
@@ -1,8 +1,14 @@
 package com.example.compose_basicstatecodelab.ui
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -12,9 +18,20 @@ fun WaterCounter(
     text: Int,
     modifier: Modifier = Modifier
 ) {
-    val count = 0
-    Text(
-        text = stringResource(text, count),
+    Column(
         modifier = modifier.padding(16.dp)
-    )
+    ) {
+        // Changes to count are now tracked by Compose
+        //val count: MutableState<Int> = remember { mutableStateOf(0) }
+        var count by remember { mutableStateOf(0) }
+        Text(
+            text = stringResource(text, count)
+        )
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp)
+        ) {
+            Text("Add one")
+        }
+    }
 }
